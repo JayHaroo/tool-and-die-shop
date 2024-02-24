5ce19c916736d20c64d1b0d4776df7acf849f8e8
JCharls1 <garciajohncharles33@gmail.com>
2024-02-24 18:49:31 +0800

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Ordering from "./components/ordering";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Ordering />
+      <Login />
     </>
   );
 }
