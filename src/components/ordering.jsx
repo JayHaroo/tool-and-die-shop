c7c536b2882428f3b6d3b776ea53ddc19292c1e9
JeyHaroo <mjeyict3@gmail.com>
2024-02-27 20:01:27 +0800

    Cleaned up unused function

--- src/components/ordering.jsx
+++ src/components/ordering.jsx
@@ -52,12 +52,6 @@ export default function Ordering() {
     setCart([]);
   };
 
-  const handleAddItem = () => {
-    const newItem = { name: newItemName, id: menuItems.length + 1, quantity: 0 };
-    setMenuItems([...menuItems, newItem]);
-    setNewItemName("");
-  };
-
   const filteredMenuItems = menuItems.filter((item) =>
     item.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -83,7 +77,7 @@ export default function Ordering() {
               <span>{item.name}</span>
               <span>Quantity: {item.quantity}</span>
               <span>Price: ${item.price}</span>
-              <button onClick={() => addItem(item)}>Add to Cart</button>
+              <button onClick={() => addItem(item)}><svg width="70px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 5L19 12H7.37671M20 16H8L6 3H3M16 5.5H13.5M13.5 5.5H11M13.5 5.5V8M13.5 5.5V3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
             </div>
           ))}
         </div>
