5e06450ace6719ce1411fe550a72f12eaf3cf426
JeyHaroo <mjeyict3@gmail.com>
2024-03-01 23:17:17 +0800

    Fixed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,6 @@ const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [adminLogin, setAdminLogin] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = () => {
